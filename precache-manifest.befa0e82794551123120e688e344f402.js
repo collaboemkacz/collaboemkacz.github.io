4341ac4f7ed369ce02af1ec4cea555598b5f0839
collaboemkacz <collabo.emkacz@gmail.com>
2024-05-22 22:18:47 +0200

    Updates

---
+++ precache-manifest.befa0e82794551123120e688e344f402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193b631f39723ffd2173a24d4fbfa929",
+    "url": "/collaboemkacz.github.io/index.html"
+  },
+  {
+    "revision": "7cb3171be2382a817a88",
+    "url": "/collaboemkacz.github.io/static/css/main.f6143e02.chunk.css"
+  },
+  {
+    "revision": "d2dfc5e6788b87403ab2",
+    "url": "/collaboemkacz.github.io/static/js/2.4b1af574.chunk.js"
+  },
+  {
+    "revision": "7cb3171be2382a817a88",
+    "url": "/collaboemkacz.github.io/static/js/main.628bbd73.chunk.js"
+  },
+  {
+    "revision": "c36bee9502246c52de29",
+    "url": "/collaboemkacz.github.io/static/js/runtime~main.c01351b3.js"
+  }
+]);
